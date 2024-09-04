bb5b347338be7cf197ab275a9ea15a0daeec8b1f
root <root@DESKTOP-RHA5CJR.localdomain>
2024-09-05 00:04:31 +0300

    attributes and properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,35 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name="Unknown", breed="Mutt"):
+        # Validate and set the name first
+        self.name = name  
+        
+        # Then validate and set the breed
+        self.breed = breed
+
+    # Getter for the name property
+    def get_name(self):
+        return self._name
+
+    # Setter for the name property with validation
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # Getter for the breed property
+    def get_breed(self):
+        return self._breed
+
+    # Setter for the breed property with validation
+    def set_breed(self, breed):
+        if breed in APPROVED_BREEDS:
+            self._breed = breed
+        else:
+            print("Breed must be in list of approved breeds.")
+
+    # Define the name and breed properties
+    name = property(get_name, set_name)
+    breed = property(get_breed, set_breed)
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,33 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name="Unknown", job="Unemployed"):
+        self.name = name  # Validate and format name
+        self.job = job  # Validate job
+
+    # Getter for the name property
+    def get_name(self):
+        return self._name
+
+    # Setter for the name property with validation and title case conversion
+    def set_name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()  # Convert to title case before setting
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    # Getter for the job property
+    def get_job(self):
+        return self._job
+
+    # Setter for the job property with validation
+    def set_job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    # Define the name and job properties
+    name = property(get_name, set_name)
+    job = property(get_job, set_job)
+
